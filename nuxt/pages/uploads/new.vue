df314797cfc7e8abb3312549ba00422c5797564f
YutaKanna <kannayuta0629@gmail.com>
2021-01-02 07:14:39 +0900

    投稿できるようにする #5

--- nuxt/pages/uploads/new.vue
+++ nuxt/pages/uploads/new.vue
@@ -13,16 +13,24 @@ import 'firebase/storage';
 export default {
   data() {
     return {
-      name: ''
+      imageName: '',
+      imageURL: ''
     }
   },
   methods: {
-    selectIcon (e) {
+    async selectIcon (e) {
         const file = e.target.files[0]
         // refの中身が保存する場所のpathになる
-        console.log(file.name);
         const storageRef = firebase.storage().ref(file.name)
-        storageRef.put(file)
+        const snapshot = await storageRef.put(file)
+        const imageURL = await snapshot.ref.getDownloadURL();
+
+        var docDataImages = {
+            imageName: file.name,
+            imageURL: imageURL,
+        };
+        var db = firebase.firestore()
+        db.collection('images').add(docDataImages)
     },
   }
 }
